0d32b89b1fe5fcb0dc2b914207ad1e75bd54496a
fearless-snippet-5362 <cpk1115@gmail.com>
2019-03-23 21:56:34 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = { artistName: "songTitle" };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-return playlist.assign({ playlist }, { artistName }, { songTitle })
+return playlist[artistName]=songTitle;
 }
   
